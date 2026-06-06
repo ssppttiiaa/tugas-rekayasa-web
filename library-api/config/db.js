447330c65eb889458cba1ceb9c2b58bf79419d9f
septiaaa <Septianapujiastuti15@gmail.com>
2026-06-06 11:52:09 +0700

    update db config for aiven

--- library-api/config/db.js
+++ library-api/config/db.js
@@ -1,18 +1,22 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "library_db"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Database Connected");
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Database connected");
+  }
 });
 
 module.exports = db;
